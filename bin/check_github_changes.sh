eb7580e162fce7cd33f47770c9219e65daf370b1
bytesmith-ahmad <b-9k@outlook.com>
2024-04-06 17:54:43 -0400

    Fixed gitignore, now all is ignored except whitelisted dirs

---
+++ bin/check_github_changes.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Define your GitHub repository URL
+github_repo="$github/home"
+
+# Check for changes in the remote repository
+git remote update
+
+# Check if there are any changes
+if git status -uno | grep -q 'Your branch is behind'; then
+    # Changes found, prompt the user
+    read -p "Found changes in the remote repository. Do you want to pull? (Y/N): " answer
+    if [[ "$answer" =~ ^[Yy]$ ]]; then
+        # User confirmed, pull changes
+        git pull "$github_repo"
+    else
+        echo "Changes not pulled."
+    fi
+else
+    echo "No changes found in the remote repository."
+fi
+
+#track me!
