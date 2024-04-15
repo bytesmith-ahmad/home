58dd72f520d88ea3187fa079bceb8f2ea102985a
bytesmith-ahmad <b-9k@outlook.com>
2024-04-15 12:32:07 -0400

    anniv passed all tests! Fully functional, can now run SQL queries from command line

--- bin/enhance-table.pl
+++ bin/enhance-table.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 # ANSI color escape codes
-my $G = "\e[48;5;236m"; # Dark gray color
+my $G = "\e[48;2;28;28;28m"; # Dark gray color, same as TaskWarrior (1c1c1c)
 my $R = "\e[0m";        # Reset color
 my $Y = "\e[1;33m";     # Yellow color for the header
 
@@ -15,16 +15,20 @@ while (<STDIN>) {
     push @table, $_;
 }
 
-# Print only the header (first row) of the table with color
-print $Y . $table[0] . $R . "\n";
-print $table[1]."\n";
-
-# Print the rest of the table
-for (my $i = 2; $i < @table; $i++) {
-    # Print the row with color
-    if ($i % 2 == 0) {
-        print $table[$i] . "\n";
-    } else {
-        print $G . $table[$i] . $R . "\n";
+if (@table > 3){
+    print $Y.$table[0].$R."\n";  # Print only the header (first row) of the table with color
+    print $table[1]."\n";         # Print the second line
+    # Print the rest of the table
+    for (my $i = 2; $i < @table; $i++) {
+        # Print the row with color
+        if ($i % 2 == 0) {
+            print $table[$i] . "\n";
+        } else {
+            print $G . $table[$i] . $R . "\n";
+        }
+    }
+} else {  # If table size is less than 4 rows, skip coloring
+    foreach my $row (@table) {
+        print "$row\n";
     }
 }
