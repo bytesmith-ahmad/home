a2c3b66193dcb299ba57797ccf22fc0a28cee250
bytesmith-ahmad <b-9k@outlook.com>
2024-04-30 01:21:03 -0400

    minor edit to startup

--- bin/startup.sh
+++ bin/startup.sh
@@ -22,9 +22,9 @@ print_color() {
 
 # Display date and time with decoration
 echo ""
-print_decoration "*" 50
-print_color "Current date and time: $(date)" "1;36"
-print_decoration "*" 50
+print_decoration "*" 28
+print_color "$(date)" "1;36"
+print_decoration "*" 28
 
 # Display tasks from taskwarrior
 task summary
@@ -32,9 +32,9 @@ task context none >/dev/null 2>&1
 task limit:5
 
 # Display guidance message with decoration
-echo ""
-print_decoration "-" 50
-print_color "For guidance: enter 'guide'" "1;33"
-#print_color "To view list of commands: 'ls -1 bin'" "1;33"
-print_decoration "-" 50
-echo ""
+echo -e "\e[33mConsider a 'help' command in case I forget what to do\e[0m"
+# print_decoration "-" 50
+# print_color "For guidance: enter 'guide'" "1;33"
+# #print_color "To view list of commands: 'ls -1 bin'" "1;33"
+# print_decoration "-" 50
+# echo ""
